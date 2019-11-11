ca13856825a27c32b83383b7fb975ff85de23a06
Jesse Anim <iamjesse75@gmail.com>
2019-11-11 16:20:35 +0000

    Initial commit

---
+++ app/src/main/java/com/tekdivisal/safet/MainActivity.java
@@ -0,0 +1,123 @@
+package com.tekdivisal.safet;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FragmentManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        manager = getSupportFragmentManager();
+
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        manager.beginTransaction().replace(R.id.container, new Home()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the MainActivity/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.notifications) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            manager.beginTransaction().replace(R.id.container, new Home()).commit();
+        }
+        else if (id == R.id.profile) {
+            manager.beginTransaction().replace(R.id.container, new Profile()).commit();
+
+        } else if (id == R.id.edit_location) {
+            manager.beginTransaction().replace(R.id.container, new Edit_Location()).commit();
+
+        } else if (id == R.id.settings) {
+            manager.beginTransaction().replace(R.id.container, new Settings()).commit();
+
+        } else if (id == R.id.about_school) {
+            manager.beginTransaction().replace(R.id.container, new About()).commit();
+
+        } else if (id == R.id.contact) {
+            manager.beginTransaction().replace(R.id.container, new Contact()).commit();
+
+        }
+        else if (id == R.id.logout) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/tekdivisal/safet/Settings.java
@@ -0,0 +1,55 @@
+package com.tekdivisal.safet;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Settings extends Fragment {
+    private CheckBox pickup_notify, reached_notify, left_notify, dropped_notify;
+
+
+    public Settings() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View settings =  inflater.inflate(R.layout.fragment_settings, container, false);
+
+        pickup_notify = settings.findViewById(R.id.pickup_notify_text);
+        reached_notify = settings.findViewById(R.id.reached_notify_text);
+        dropped_notify = settings.findViewById(R.id.drop_notify_text);
+        left_notify = settings.findViewById(R.id.left_notify_text);
+
+        //        making the text color mix mix
+//        for pickup notification text
+        Spannable wordtoSpan = new SpannableString(getResources().getString(R.string.pickup_notification));
+        wordtoSpan.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.main_blue)), 0, 20, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        pickup_notify.setText(wordtoSpan);
+
+//        for drop notification
+
+//        for reached notification
+
+//        for left school notification
+        return settings;
+
+    }
+
+}
