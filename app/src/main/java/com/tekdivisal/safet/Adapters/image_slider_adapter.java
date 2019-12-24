4d6f8c1dc48bbbfe233c7837c29813c9fd76f239
Jesse Anim <iamjesse75@gmail.com>
2019-12-24 19:00:51 +0000

    petty changes

--- app/src/main/java/com/tekdivisal/safet/Adapters/image_slider_adapter.java
+++ app/src/main/java/com/tekdivisal/safet/Adapters/image_slider_adapter.java
@@ -14,13 +14,13 @@ public class image_slider_adapter extends SliderAdapter {
     public void onBindImageSlide(int position, ImageSlideViewHolder viewHolder) {
         switch (position) {
             case 0:
-                viewHolder.bindImageSlide("https://assets.materialup.com/uploads/dcc07ea4-845a-463b-b5f0-4696574da5ed/preview.jpg");
+                viewHolder.bindImageSlide("https://www.cdnis.edu.hk/sites/default/files/styles/1200x795/public/CDNIS%20solar%20panel_1.jpg?itok=qCVODOAq");
                 break;
             case 1:
-                viewHolder.bindImageSlide("https://assets.materialup.com/uploads/20ded50d-cc85-4e72-9ce3-452671cf7a6d/preview.jpg");
+                viewHolder.bindImageSlide("https://studysive.com/wp-content/uploads/2019/08/IMG_6482av-cropped.jpg");
                 break;
             case 2:
-                viewHolder.bindImageSlide("https://assets.materialup.com/uploads/76d63bbc-54a1-450a-a462-d90056be881b/preview.png");
+                viewHolder.bindImageSlide("https://www.al.com/resizer/ceGxncEJ-FXZLbQo_kWb9aCOUKM=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.al.com/home/bama-media/width2048/img/spotnews/photo/gardendale-high-school-d22ee872679c54d7.jpg");
                 break;
         }
     }
